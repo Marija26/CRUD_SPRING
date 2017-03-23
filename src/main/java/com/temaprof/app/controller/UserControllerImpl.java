de524c98cf35847beae7877d14ee9b79f9e7c206
temaprof <temaprofit@gmail.com>
2017-03-23 14:06:44 +0200

    Add_User_ControllerImpl

--- src/main/java/com/temaprof/app/controller/UserControllerImpl.java
+++ src/main/java/com/temaprof/app/controller/UserControllerImpl.java
@@ -1,37 +1,69 @@
 package com.temaprof.app.controller;
 
+import com.temaprof.app.model.User;
 import com.temaprof.app.repository.UserRepository;
+import com.temaprof.app.validation.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * Created by TEMAPROF on 20.03.2017.
  */
-public class UserControllerImpl implements UserController{
+@Controller
+public class UserControllerImpl implements UserController {
+
     private UserRepository repository;
 
+    private UserValidator validator;
+
+    @Autowired
+    public UserControllerImpl(UserRepository repository, UserValidator validator) {
+        this.repository = repository;
+        this.validator = validator;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     @Override
     public String getUsers(ModelMap model) {
+        List<User> users = this.repository.listAll();
+        model.addAttribute("user", users);
         return null;
     }
 
+    @RequestMapping(value = "addUser", method = RequestMethod.GET)
     @Override
     public String addUser(ModelMap model) {
-        return null;
+        return "addUser";
     }
 
+    @RequestMapping(value = "addUser", method = RequestMethod.POST)
     @Override
-    public String addUser(String name, String age,  Boolean isAdmin) {
-        return null;
+    public String addUser(@RequestParam("name") String name, @RequestParam("age") String age, @RequestParam(value = "isAdmin",
+    required = true, defaultValue = "false") Boolean isAdmin) {
+        User user = new User(name, Integer.parseInt(age), isAdmin);
+        this.repository.addUser(user);
+        return "redirect:/";
     }
 
+    @RequestMapping(value = "deleteUser/{id}", method = RequestMethod.GET)
     @Override
-    public String deleteUser(Integer id) {
-        return null;
+    public String deleteUser(@PathVariable Integer id) {
+        this.repository.removeUser(id);
+        return "redirect:/";
     }
 
+    @RequestMapping(value = "updateUser/{id}", method = RequestMethod.GET)
     @Override
-    public String getUpdate(Integer id, Model model) {
-        return null;
+    public String getUpdate(@PathVariable Integer id, Model model) {
+        model.addAttribute("userAttribute", this.repository.get(id));
+        return "updateUser";
     }
 }
