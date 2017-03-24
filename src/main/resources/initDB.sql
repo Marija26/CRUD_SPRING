37c5bd560f8b7c16813ac1d5188c28db39bc6cbf
temaprof <temaprofit@gmail.com>
2017-03-24 13:30:24 +0200

    Add_initDB

---
+++ src/main/resources/initDB.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS user;
+DROP SEQUENCE IF EXISTS global_seq;
+
+CREATE SEQUENCE global_seq START 100000;
+
+CREATE TABLE users
+(
+  id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  age        INTEGER ,
+  name       VARCHAR NOT NULL,
+  createdDate TIMESTAMP DEFAULT now(),
+  isAdmin    BOOL DEFAULT TRUE
+);
+
